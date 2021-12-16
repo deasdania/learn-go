97e976ec4d1f81b42eb258476417131b90baf138
dea dania <deadania@deas-MacBook-Pro.local>
2021-12-16 08:43:36 +0700

    partial html template with template.ParseGlob

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+type M map[string]interface{}
+
+func main() {
+	var tmp, err = template.ParseGlob("views/*")
+	if err != nil {
+		panic(err.Error())
+		return
+	}
+
+	http.HandleFunc("/index", func(rw http.ResponseWriter, r *http.Request) {
+		var data = M{"name": "Batman"}
+		err = tmp.ExecuteTemplate(rw, "index", data)
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	http.HandleFunc("/about", func(rw http.ResponseWriter, r *http.Request) {
+		var data = M{"name": "Batman"}
+		err = tmp.ExecuteTemplate(rw, "index", data)
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	fmt.Println("server started at localhost:9000")
+	http.ListenAndServe(":9000", nil)
+}
